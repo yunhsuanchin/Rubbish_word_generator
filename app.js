ddbf746a0bac017cf24a0c266f1a73fd1f8795e8
yunhsuanchin <alicechin20181@gmail.com>
2020-10-05 21:27:52 +0800

    writing main.handlebars & index.handlebars

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-// require necessary modules in this project
+// require necessary modules and define server related variables in this project
 const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
 
 // setting routes
 app.get('/', (req, res) => {
